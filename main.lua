acf9fe0f63bd157d99bd6d9d30f1d2ac98b6cc36
axorkgamecodeur <axorkgamecodeur@gmail.com>
2020-12-12 00:50:53 +0100

    ExoVariable

--- main.lua
+++ main.lua
@@ -7,3 +7,16 @@ print("Nombre de vies contient : ", nombreDeVies)
 nombreDeVies = 4
 
 print("Nombre de vies contient : ", nombreDeVies)
+
+-- float
+hauteur = 10.5
+
+-- bool
+estEnVeille = true
+estEnVeille = false
+
+-- string
+titre = "Redemption of Sam"
+texteInterface = "Nombre de vies"
+
+-- pas d'exercice sur les variables 
