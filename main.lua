c47ff62bfa065afbda71a9141e7133de2cd6cd44
axorkgamecodeur <axorkgamecodeur@gmail.com>
2020-12-12 01:10:53 +0100

    ajout de la partie expression

--- main.lua
+++ main.lua
@@ -20,3 +20,20 @@ titre = "Redemption of Sam"
 texteInterface = "Nombre de vies"
 
 -- pas d'exercice sur les variables 
+
+-- expressions
+
+variable = 10 + 3
+
+print(variable % 5)
+
+score = 0
+
+print("Nombre de vies restantes "..nombreDeVies)
+chaine1 = "Test 1"
+chaine2 = "Test 2"
+nouvelleChaine = chaine1..chaine2
+
+-- expression complexe
+resultat = 10 * 20 / 5 + 1
+print(resultat)
